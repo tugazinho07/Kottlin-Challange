8629e995b1fb32f7ec9f2fc806f69ad494123bea
tugazinho07 <a22003736@alunos.ulht.pt>
2023-03-26 19:12:11 +0100

    fim?

--- src/pt/ulusofona/cm/kotlin/challenge/Main.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/Main.kt
@@ -1,7 +1,12 @@
 package pt.ulusofona.cm.kotlin.challenge
 
+import pt.ulusofona.cm.kotlin.challenge.models.Carro
+import pt.ulusofona.cm.kotlin.challenge.models.Motor
+
 
 fun main() {
     // aqui escreves o código do programa
-
+    val car = Carro("Fusca", Motor(550,400))
+    car.moverPara(120,500)
+    println(car)
 }
--- src/pt/ulusofona/cm/kotlin/challenge/models/Carro.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Carro.kt
@@ -1,13 +1,55 @@
 package pt.ulusofona.cm.kotlin.challenge.models
 
+import pt.ulusofona.cm.kotlin.challenge.exceptions.AlterarPosicaoException
+import pt.ulusofona.cm.kotlin.challenge.exceptions.VeiculoDesligadoException
+import pt.ulusofona.cm.kotlin.challenge.exceptions.VeiculoLigadoException
 import pt.ulusofona.cm.kotlin.challenge.interfaces.Ligavel
 import java.text.SimpleDateFormat
 
-abstract class Carro(override var identificador:String, var motor:Motor) : Veiculo(identificador), Ligavel{
+    class Carro(override var identificador:String, var motor:Motor) : Veiculo(identificador), Ligavel{
 
     fun dataFormatada() : String {
         val formato = SimpleDateFormat("dd-MM-yyyy")
         val dataModificada = formato.format(dataDeAquisicao)
         return dataModificada.toString()
     }
+
+        override fun requerCarta(): Boolean {
+            return true
+        }
+
+    override fun moverPara(x: Int, y: Int) {
+
+        if (posicao.x == x && posicao.y == y) {
+            throw AlterarPosicaoException()
+        }
+
+        if (motor.estaLigado()){
+            posicao.changePosition(x, y)
+            desligar()
+        }
+    }
+
+    override fun ligar() {
+        if(estaLigado()){
+            throw  VeiculoLigadoException()
+        }
+        motor.ligar()
+    }
+
+    override fun desligar() {
+        if(!estaLigado()){
+            throw VeiculoDesligadoException()
+        }
+        motor.desligar()
+    }
+
+    override fun estaLigado(): Boolean {
+        return motor.estaLigado()
+    }
+
+    override fun toString(): String {
+        return "Carro | $identificador | ${dataFormatada()} | ${posicao.toString()}"
+    }
+
 }
